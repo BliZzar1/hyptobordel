b0cca5160c6f5de04729541f24cf77b33396eb72
boursyt <boursyt@gmail.com>
2025-03-04 22:09:48 +0100

    fix: second activity

--- app/src/main/java/com/upjv/ccm2025_helloworld/ActiviteSecondaire.java
+++ app/src/main/java/com/upjv/ccm2025_helloworld/ActiviteSecondaire.java
@@ -17,24 +17,23 @@ public class ActiviteSecondaire extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
-
-        // Methode qui créé tous les éléments de la view
         setContentView(R.layout.layout_activite_secondaire);
 
         exitButton = findViewById(R.id.id_exit_button_sc2);
         exitButton.setOnClickListener(this::clickSecondButton);
-        //Version ultra compact avec les lambdas calculs
-        //exitButton.setOnClickListener(v->{finish();});
 
-        bundlePreview.findViewById(R.id.id_bundle_extras_textview);
+        // Initialize bundlePreview
+        bundlePreview = findViewById(R.id.id_bundle_extras_textview);
 
-        // Récupéré la valeur du bundle passé dans l'intend
+        // Retrieve the value from the bundle passed in the intent
         Intent monIntent = getIntent();
         Bundle monBundle = monIntent.getExtras();
-        String laChaine = monBundle.getString("Texte");
-        bundlePreview.setText(laChaine);
+        if (monBundle != null) {
+            String laChaine = monBundle.getString("Texte");
+            bundlePreview.setText(laChaine);
+        }
     }
+
     private void clickSecondButton(View view) {
         //Logs dans logcat
         Log.d(TAG, "Activité secondaire quittée");
